328256df584ff4da7f9463f99f4b4d4d0c75b828
Boris Okunskiy <i@savant.pro>
2015-05-21 15:43:24 +0300

    Revisited hook on npm install

--- scripts/install.js
+++ scripts/install.js
@@ -4,7 +4,7 @@ var done = require('../src/utils/done')
   , git = require('../src/utils/git')
   , hook = require('../src/utils/hook')
   , colors = require('colors')
-  , which = require('which');
+  , fs = require('fs');
 
 /**
  * This script is executed by npm when the package is installed.
@@ -14,9 +14,9 @@ git.getDir(function (err, gitdir) {
     // not a git dir, don't do anything
     return done();
   }
-  which('git-alert', function (err) {
-    var cmd = err ? 'node node_modules/git-alert/bin/git-alert.js show'
-        : 'git alert show';
+  fs.stat('node_modules/git-alert', function (err) {
+    var cmd = err ? 'git alert show'
+      : 'node node_modules/git-alert/bin/git-alert.js show';
     hook('post-merge', cmd, function (err) {
       if (err) return done(err);
       process.stdout.write(colors.yellow('git-alert:') +
